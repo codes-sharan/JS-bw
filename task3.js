b0a7cdfc31dd79855cd0e48fb7d2caa068722d5d
Sharan Panthi <shpanthi09@gmail.com>
2024-10-28 09:02:24 +0545

    for loop

---
+++ task3.js
@@ -0,0 +1,41 @@
+// let i = 1;
+// for (; i <= 40; i++) {
+//   if (i % 2 == 0) {
+//     console.log(i);
+//   }
+// }
+
+// for (let j = 2; j <= 40; j += 2) {
+//   console.log(j);
+// }
+
+// for (let j = 2, i=1; i <= 40; i++,j += 2) {
+//     console.log(j);
+//   }
+
+// const numbs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// let sum = 0;
+// for (i = 0; i < numbs.length; i++) {
+//   sum += numbs[i];
+//   //   console.log(sum);
+// }
+// console.log(sum);
+
+// for (i = 1; i <= 7; i++) {
+//   let toPrint = "";
+//   for (j = 1; j <= i; j++) {
+//     // console.log(j);
+//     toPrint += j + " ";
+//   }
+//   console.log(toPrint);
+// }
+
+for (i = 1; i <= 5; i++) {
+  let toPrint = "";
+  for (j = 1; j <= i; j++) {
+    // console.log(j);
+    toPrint += "*   ";
+  }
+  console.log(toPrint);
+}

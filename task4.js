f1149eb084e6de5934ec5d830071959812126032
Sharan Panthi <shpanthi09@gmail.com>
2024-10-29 23:02:17 +0545

    file names changed

--- task4.js
+++ task4.js
@@ -1,48 +1,61 @@
-const fullMarks = 500;
-
-const studentScores = [
-  {
-    name: "Hari",
-    email: "hari@gmail.com",
-    marks: 387,
-  },
-
-  {
-    name: "Ram",
-    email: "ram@gmail.com",
-    marks: 427,
-  },
-
-  {
-    name: "Gita",
-    email: "gita@gmail.com",
-    marks: 347,
-  },
+const employees = [
+  { name: "Alice Johnson", annualIncome: 450000 },
+  { name: "Bob Smith", annualIncome: 1200000 },
+  { name: "Charlie Brown", annualIncome: 750000 },
+  { name: "Diana Prince", annualIncome: 2000000 },
+  { name: "Edward Elric", annualIncome: 300000 },
+  { name: "Fiona Apple ", annualIncome: 1800000 },
+  { name: "George Washington", annualIncome: 900000 },
+  { name: "Hannah Montana", annualIncome: 2100000 },
+  { name: "Ian Fleming", annualIncome: 600000 },
+  { name: "Julia Roberts", annualIncome: 2700000 },
 ];
 
-for (scores of studentScores) {
-  const perc = (scores.marks * 100) / fullMarks;
-
-  if (scores.marks <= fullMarks) {
-    if (perc >= 80) {
-      div = "Distinction";
-    } else if (perc >= 60) {
-      div = "First Division";
-    } else if (perc >= 45) {
-      div = "Second Division";
-    } else if (perc >= 32) {
-      div = "Third Division";
-    } else {
-      div = "Failed";
-    }
+for (employee of employees) {
+  if (employee.annualIncome <= 600000) {
+    employee.yearlyTax = (employee.annualIncome * 1) / 100;
+  } else if (employee.annualIncome <= 800000) {
+    employee.yearlyTax =
+      (600000 * 1) / 100 + (employee.annualIncome - 600000) * 0.15;
+  } else if (employee.annualIncome <= 1100000) {
+    employee.yearlyTax =
+      (600000 * 1) / 100 +
+      200000 * 0.15 +
+      (employee.annualIncome - 800000) * 0.2;
+  } else if (employee.annualIncome <= 1600000) {
+    employee.yearlyTax =
+      (600000 * 1) / 100 +
+      200000 * 0.15 +
+      300000 * 0.2 +
+      (employee.annualIncome - 1100000) * 0.25;
+  } else if (employee.annualIncome <= 2600000) {
+    employee.yearlyTax =
+      (600000 * 1) / 100 +
+      200000 * 0.15 +
+      300000 * 0.2 +
+      500000 * 0.25 +
+      (employee.annualIncome - 1600000) * 0.3;
+  } else if (employee.annualIncome > 2600000) {
+    employee.yearlyTax =
+      (600000 * 1) / 100 +
+      200000 * 0.15 +
+      300000 * 0.2 +
+      500000 * 0.25 +
+      1000000 * 0.3 +
+      (employee.annualIncome - 2600000) * 0.36;
   } else {
-    console.log("Invalid marks: Marks should be <= fullMarks.");
+    console.log("Invalid annual income");
   }
 
-  console.log(`Name ${scores.name}
-Email: ${scores.email} 
-Marks Obtained: ${scores.marks}
-Percentage: ${perc}
-Division: ${div}
-------------------------`);
+  employee.monthlyTax = employee.yearlyTax / 12;
+  employee.annualNetSalary = employee.annualIncome - employee.yearlyTax;
+  employee.monthlyNetSalary = employee.annualNetSalary / 12;
+
+  console.log(`Name ${employee.name}
+        Annual Income: Npr: ${employee.annualIncome} 
+        Monthly Tax: Npr ${employee.monthlyTax}
+        Yearly Tax: Npr ${employee.yearlyTax}
+        Monthly Net Salary: Npr ${employee.monthlyNetSalary}
+        Annual Net Salary: Npr ${employee.annualNetSalary}
+        ------------------------`);
 }
